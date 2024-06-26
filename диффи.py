20ef21bfe65aadc2f164bd9db5ffef4fd447cbf9
il0vesleep <105927047+il0vesleep@users.noreply.github.com>
2024-06-26 17:16:11 +0300

    Update диффи.py
    Изменил оформление

--- диффи.py
+++ диффи.py
@@ -1,11 +1,12 @@
-g=int(input())
-p=int(input())
-a=int(input())
-b=int(input())
-x=6789127596547
+g=int(input("Введите g "))
+p=int(input("Введите p "))
+a=int(input("Введите a "))
+b=int(input("Введите b "))
 A=(g**a)%p
 B=(g**b)%p
 ka=(A**b)%p
 kb=(B**a)%p
 k=(g**(a*b))%p
-print(ka,kb,k)
+print("Ключ А ", ka,)
+print("Ключ Б ", kb)
+print("Общий ключ ", k)
