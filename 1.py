7567678e53fff2b8b9b6f6e781c02c68cceff60d
il0vesleep <105927047+il0vesleep@users.noreply.github.com>
2024-06-26 11:01:31 +0300

    Update 1.py
    исправление ошибок

--- 1.py
+++ 1.py
@@ -1,25 +1,28 @@
+lst=[]
+for i in range (2,100):
+    for j in range(2, i):
+        if i % j == 0:
+            break
+    else:
+        lst.append(i)
+print(lst)
 p= int(input())
 q= int(input())
 n=p*q
 f=(p-1)*(q-1)
-e=0
-b=0
-for i in range(2,f):
-    if f % i == 1 and b ==0:
-        e=i
-        b=i
-    elif (f % i == 1) and b !=0:
-        b=i
+m=0
+for x in lst:
+    if f % x !=0:
+        e=x
+        print("e",e)
+        break
+for da in lst:
+    if da *e %f==1 and da!=e:
+        d=da
+        print(d)
+        break
 p1=int(input())
 s=(p1**e)%n
-a = (s**b)%n
-print(e,b)
+a = (s**d)%n
 print (p,q)
 print (s,a)
-
-
-
-
-
-
-
